8779279a64e5b521519810d0fec7cc657970ba16
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-06-09 18:24:28 +0500

    попап открывается вместе с самим сайтом, надо исправить!

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const popupElement = document.querySelector('.popup');
-const popupButtonOpened = document.querySelector('.profile__button-edit');
-const popupButtonClosed = document.querySelector('.popup__button-close');
+const buttonEdit = document.querySelector('.profile__button-edit');
+const buttonClose = document.querySelector('.popup__button-close');
 
-function handleClick() {
-  popupElement.classList.add('profile__button-edit')
+function popupOpen() {
+  popupElement.classList.add('popup__opened');
 }
 
-popupButtonOpened.addEventListener('click', handleClick)
+function popupClose(e) {
+
+  if (e.target === e.currentTarget) {
+    popupElement.classList.remove('popup__opened');
+  }
+}
+
+buttonEdit.addEventListener('click', popupOpen);
+
+buttonClose.addEventListener('click', popupClose);
+popupElement.addEventListener('click', popupClose);

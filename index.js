57b3e63af98ac7746bbaf2ec40243b81f9857c30
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-06-10 18:02:15 +0500

    делаю лайк/дизлайк

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const popupElement = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__button-close');
+const profileContainer = document.querySelector('.profile__container');
+const profileButtonSave = document.querySelector('.profile__button');
+let groupButton = document.querySelector('.group__button');
 
+// Закрываем и открываем попап
 function popupOpen() {
   popupElement.classList.add('popup__opened');
 }
@@ -17,3 +21,18 @@ buttonEdit.addEventListener('click', popupOpen);
 
 buttonClose.addEventListener('click', popupClose);
 popupElement.addEventListener('click', popupClose);
+
+// end
+
+// Ставим/убираем лайк
+
+function groupLike() {
+  groupButton.classList.toggle('group__button_active');
+}
+
+groupButton.addEventListener('click', groupLike);
+
+function addInfoContainer() {
+  let profileName = document.querySelector('.profile__title');
+  let profileInfo = document.querySelector('.profile__subtitle');
+}

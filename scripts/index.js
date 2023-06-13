91ff90a5314cb5640fe5847703b62ad6813bb850
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-06-14 00:57:29 +0500

    Вторая итерация, вроде исправил все замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,35 @@
 const popupElement = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__button-close');
-const buttonSave = document.querySelector('.popup__button');
 
 let nameEdit = document.querySelector('.profile__title');
 let infoEdit = document.querySelector('.profile__subtitle');
-let inputEditName = document.querySelector('.popup__edit-name');
-let inputEditInfo = document.querySelector('.popup__edit-info');
+let inputEditName = document.querySelector('.popup__edit_name');
+let inputEditInfo = document.querySelector('.popup__edit_info');
+let formElement = document.querySelector('.popup__form')
 
 // Закрываем и открываем попап
 function popupOpen() {
-  popupElement.classList.add('popup__opened');
-  console.log(buttonEdit);
-  console.log(buttonSave);
+  popupElement.classList.add('popup_opened');
+
 }
 
 function popupClose(e) {
 
   if (e.target === e.currentTarget) {
-    popupElement.classList.remove('popup__opened');
+    popupElement.classList.remove('popup_opened');
   }
+}
+// end 
+
+function editProfile() {
+  nameEdit.textContent = inputEditName.value;
+  infoEdit.textContent = inputEditInfo.value;
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  editProfile();
 }
 
 buttonEdit.addEventListener('click', popupOpen);
@@ -27,13 +37,18 @@ buttonEdit.addEventListener('click', popupOpen);
 buttonClose.addEventListener('click', popupClose);
 popupElement.addEventListener('click', popupClose);
 
+<<<<<<< HEAD:scripts/index.js
+formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', popupClose);
+=======
 // end 
 
 function editProfile() {
-  nameEdit.innerHTML = inputEditName.value;
-  infoEdit.innerHTML = inputEditInfo.value;
+  nameEdit.textContent = inputEditName.value;
+  infoEdit.textContent = inputEditInfo.value;
 }
 
 // Кнопка "Сохранить" закрывает попап
 buttonSave.addEventListener('click', popupClose);
 buttonSave.addEventListener('click', editProfile);
+>>>>>>> f66702c7cb44e18050c40c75bde863471edf34ff:index.js

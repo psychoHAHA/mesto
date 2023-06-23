69bc8c6ea02c37dcfab0060f3804aeef412fdcc9
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-06-23 20:02:41 +0500

    5 проектная работа. Начало

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 // Переменные
 
 const popupElement = document.querySelector('.popup');
+const popupImageElement = document.querySelector('.popup-image');
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__button-close');
+const buttonImageClose = document.querySelector('.popup-image__button-close')
+const buttonAdd = document.querySelector('.profile__button');
 
 let nameEdit = document.querySelector('.profile__title');
 let infoEdit = document.querySelector('.profile__subtitle');
 let inputEditName = document.querySelector('.popup__input_edit_profile-name');
 let inputEditInfo = document.querySelector('.popup__input_edit_profile-info');
-let formElement = document.querySelector('.popup__form')
+let formElement = document.querySelector('.popup__form');
 
-// Закрываем и открываем попап
+// Закрываем и открываем попапы
+
 function popupOpen() {
   popupElement.classList.add('popup_opened');
   nameEdit.textContent = inputEditName.value;
@@ -23,6 +27,20 @@ function popupClose(e) {
     popupElement.classList.remove('popup_opened');
   }
 }
+
+// ПОПАП ДОБАВЛЕНИЯ КАРТИНКИ
+
+function popupImageOpen() {
+  popupImageElement.classList.add('popup-image_opened');
+}
+
+function popupImageClose(e) {
+
+  if (e.target === e.currentTarget) {
+    popupImageElement.classList.remove('popup-image_opened');
+  }
+}
+
 // end 
 
 // Редактируем профиль 
@@ -36,12 +54,18 @@ function handleFormSubmit (evt) {
 
 // end 
 
+// Ставим лайк
+
 
 // Подключаем функции 
 
-buttonEdit.addEventListener('click', popupOpen);
+buttonEdit.addEventListener('click', popupOpen)
+buttonAdd.addEventListener('click', popupImageOpen);
 
 buttonClose.addEventListener('click', popupClose);
 popupElement.addEventListener('click', popupClose);
+
+buttonImageClose.addEventListener('click', popupImageClose);
+popupImageElement.addEventListener('click', popupImageClose);
 
 formElement.addEventListener('submit', handleFormSubmit);

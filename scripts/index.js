c7077906a4df5f7bc099d22086ba3248409d43d1
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-06-26 01:07:31 +0500

    ничего пока не сделал (

--- scripts/index.js
+++ scripts/index.js
@@ -56,8 +56,54 @@ function handleFormSubmit (evt) {
 
 // Ставим лайк
 
-const groupElement = document.querySelector('.group');
-const likeButton = groupElement.querySelectorAll('.group__button');
+// Добавляем картинки
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const templateElement = document.querySelector('#template-element');
+const groupElement = templateElement.cloneNode(true);
+const groupImage = groupElement.querySelector('.group__image');
+const buttonDelete = groupElement.querySelector('.group__button-delete');
+const groupTitle = groupElement.querySelector('.group__title');
+const buttonLike = groupElement.querySelector('.group__button');
+
+function groupAdd({name, link}) {
+  groupTitle.textContent = name;
+  groupImage.src = link;
+
+  buttonLike.addEventListener('click', () => {
+    buttonLike.classList.toggle('group__button_active');
+  });
+
+
+}
+
+buttonAdd.addEventListener('click', groupAdd);
 
 // Подключаем функции 
 

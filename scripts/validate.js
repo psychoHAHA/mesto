07d262434ef3ffc5930bb71021fae3613929b6b0
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-07-16 16:31:16 +0500

    первая итерация

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,9 +1,13 @@
+// Показываем ошибку
+
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
   inputElement.classList.add('popup__input_type_error')
   errorElement.textContent = errorMessage
   errorElement.classList.add('form__input-error_active')
 }
+
+// Прячем ошибку
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
@@ -12,6 +16,8 @@ const hideInputError = (formElement, inputElement) => {
   errorElement.textContent = ''
 }
 
+// Проверяем поля на наличие ошибок
+
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage)
@@ -19,6 +25,8 @@ const checkInputValidity = (formElement, inputElement) => {
     hideInputError(formElement, inputElement)
   }
 }
+
+// Добавляем класс disabled кнопке 
 
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
@@ -30,11 +38,15 @@ const toggleButtonState = (inputList, buttonElement) => {
   }
 }
 
+// Проходим по массиву с полями ввода при помощи some 
+
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid
   })
 }
+
+// Проходим по массивам с полями ввода при помощи forEach и навешиваем на них слушатель
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
@@ -48,6 +60,8 @@ const setEventListeners = (formElement) => {
     })
   })
 }
+
+// Проходим по массиву с формами и отменяем событие submit
 
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.popup__form'))

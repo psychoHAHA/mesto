d7ad7be4beb1a29c5526c70ff13c3aa0cb0a78e0
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-08-15 19:32:55 +0500

    Card

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import Card from "./Card.js"
+import Card from "./card.js"
 import FormValidator from "./FormValidator.js"
 import { VALIDATION_CONFIG, initialCards } from "./constants.js"
 import Popup from "./Popup.js"

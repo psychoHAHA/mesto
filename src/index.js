9c48c6d9d45db54b79024a3890a7d8918f24aea2
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-08-20 21:33:36 +0500

    первая итерация

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '../pages/index.css'
 import Card from "../components/Card.js"
 import FormValidator from "../components/FormValidator.js"
 import { VALIDATION_CONFIG, initialCards, popupProfileElement, popupCardElement, buttonEdit, buttonAdd} from "../utils/constants.js"
